a9b39f421ad131e5ae109e286c6349f0694accf9
KripaVS <kripa.ev@inapp.com>
2022-08-23 14:42:27 +0530

    phonebook socket assignemnt

--- assignments/phoneclient.py
+++ assignments/phoneclient.py
@@ -1,22 +1,117 @@
-#imorting socket module
+import json
 import socket
+import re
+
+phoneNumbers = {}
+
+class Utils:
+    def getInt(*msg):
+        while(True):
+            try:
+                value = int(input(*msg))
+                return value
+            except:
+                print("Enter a valid number")
+                continue
+
+    def getNumber(*msg):
+        while(True):
+                value = input(*msg)
+                if re.match(r'[0-9]*', value):
+                    return value
+                else:
+                    print("Enter a valid number")
+                    continue
+
+
+def list():
+    msgJson = {"ch" : 1, "args":None}
+    return msgJson
+
+def add():
+    msg = {}
+    name = input("Enter name: ")
+    phoneNumber = Utils.getNumber("Enter Phone Number: ")
+    msgJson = {"ch":2, "args":{"name":name,"phn":phoneNumber}}
+    return msgJson
+
+
+def delete():
+    nameToBeDeleted = input("Enter the name to be deleted: ")
+    msgJson = {"ch":3, "args":{"name":nameToBeDeleted}}
+    return msgJson
+
+def searchByName():
+    name = input("Enter the name to be searched: ")
+    msgJson = {"ch":4, "args":{"name":name}}
+    return msgJson
+
+def searchbyNumber():
+    phn = Utils.getNumber("Enter the phone number: ")
+    msgJson = {"ch":5, "args":{"phn":phn}}
+    return msgJson
+
+
+def actionToSelectedOption(ch):
+    global msg
+    msg = ""
+    match(ch):
+        case 1: msg = list()
+        case 2: msg = add()
+        case 3: msg = delete()
+        case 4: msg = searchByName()
+        case 5: msg = searchbyNumber()
+        case 6: msg = exit()
+        case _: print("Wrong choice")
+        
+    return json.dumps(msg)
+            
+def handleReply(msgString):
+    msg = json.loads(msgString)
+    print(msg['status'])
+    if (msg['value']):
+        print(msg['value'])
 
 def client_program():
-    host=socket.gethostname() 
-    port=5000 
-
+    host = socket.gethostname() #get the hostname
+    #since both server and client are in the same machine we can get the loopback address as the server addres
+    port = 5000 #initiate port no above 1024 till 65535
+    #HOST = "127.0.0.1" #statndard loopback interface address ( or localhost)
+    #PORT = 65432 #port to listen on (non-priveleged ports are > 1024)
     
-    client_socket=socket.socket()
-    client_socket.connect((host,port))
-    msg=input("enter the msg to send to server:")
+    #get instance of socket
+    client_socket = socket.socket() 
 
-    while msg.lower().strip()!="exit":
-        client_socket.send(msg.encode()) 
-        data=client_socket.recv(1024).decode()
-        print("received frm server: "+data) 
-        msg=input("Enter msg to send to server: ")
+    #instead of binding, client we are connecting to server
+    client_socket.connect((host,port)) #host & port as tuple
+    
+    #getting the msg to send to the server
 
+    while True:
+        ch = Utils.getInt("""
+    Enter the choice:
+    1) List all contacts
+    2) Add a new contact
+    3) Delete a contact
+    4) Search by name
+    5) Search by number
+    6) exit
+    """)
+        message = actionToSelectedOption(ch)
+        if ch == 6:
+            break
+        #if the msg is not 'exit', encode and send it to server
+        client_socket.send(message.encode())
+        #receive any reply from the server
+        data = client_socket.recv(1024).decode()
+        #printing the received data as text
+        handleReply(data)
+        #getting the msg to send to the server
+        
+
+    #close the socket connection once the while loop is exited
+    print("closing connection")
     client_socket.close()
 
-if __name__=="__main__":
+if __name__ == '__main__': 
     client_program()
--- assignments/phoneserver.py
+++ assignments/phoneserver.py
@@ -1,29 +1,99 @@
-#imorting socket module
 import socket
+import json
+phoneNumbers = {}
 
+def add(args):
+    name = args['name']
+    phoneNumber = args['phn']
+    phoneNumbers[name] = phoneNumber 
+    msg = json.dumps({"status" : "Contact added successfully",
+    "value" :None})
+    return msg
+    
+
+def list():
+    value = ""
+    for i,x in enumerate(phoneNumbers):
+        value += "\n{}. Name: {}\nPhonenumber: {}".format(i+1,x,phoneNumbers[x])
+    msg = json.dumps({"status" : "Found Contacts",
+    "value" :value})
+    return msg
+    
+def delete(args):
+    nameToBeDeleted = args['name']
+    if nameToBeDeleted in phoneNumbers.keys():
+        del phoneNumbers[nameToBeDeleted]
+        status = "Number Deleted!"
+    else: 
+        status = "Name not found"
+    msg = json.dumps({"status" : status,
+    "value" :None})
+    return msg
+
+def searchByName(args):
+    name = args['name']
+    if name in phoneNumbers.keys():
+        status = "Name found"
+        value = "Name: {}\nPhone Number: {}".format(name,phoneNumbers[name])
+    else:
+        status = "Name not found"
+        value = None
+    msg = json.dumps({"status" : status,
+    "value" :value})
+    return msg
+
+
+def searchbyNumber(args):
+    phn = args['phn']
+    if phn in phoneNumbers.values():
+        for name,phn_number in phoneNumbers.items():
+            if phn_number == phn:
+                status = "Name found"
+                value = "Name : {}\nPhone Number: {}".format(name,phn_number)
+    else:
+        status = "Phone number not found"
+        value = None
+    msg = json.dumps({"status" : status,
+    "value" :value})
+    return msg
+
+def actionToSelectedOption(ch, args):
+    global msg
+    msg = ""
+    match(ch):
+        case 1: msg = list()
+        case 2: msg =  add(args)
+        case 3: msg = delete(args)
+        case 4: msg = searchByName(args)
+        case 5: msg = searchbyNumber(args)
+        case 6: msg = exit()
+        case _: msg =print("Wrong choice")
+    return msg   
+
+def handlerequest(jsonString):
+    message = json.loads(jsonString)
+    msg = actionToSelectedOption(message["ch"],message["args"],)
+    return msg
+
+    
 def server_program():
-    host=socket.gethostname() #getting ip of host
-    port=5000
-
-    server_socket=socket.socket()
+    host = socket.gethostname() # getting the ip of host
+    port = 5000
+    server_socket = socket.socket()
     server_socket.bind((host,port))
     server_socket.listen(2)
-
-    conn,address=server_socket.accept()
-    print("Connection accepted from "+str(address))
-
-    while True: 
-        data=conn.recv(1024).decode() 
-        if not data: 
+    conn, address = server_socket.accept()
+    print("Connection accepted from " + str(address))
+    while True:
+        data = conn.recv(1024).decode()
+        if not data:
             break
-        print("message frm client "+str(address)+" : "+str(data))
-        data=input("Type reply here: ")
-        conn.send(data.encode())
-    
+        print("Message from client" + str(address) + " -> " + str(data))
+        msg = handlerequest(str(data))
+        conn.send(msg.encode())
     conn.close()
 
-if __name__=="__main__":
+if __name__ == '__main__':
     server_program()
-
 
 
